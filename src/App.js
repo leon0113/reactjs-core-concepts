39610f49de72be8cbbe2bceb272fe41718c7b5a0
leon0113 <tahjib.leon399@gmail.com>
2023-01-04 23:17:25 +0600

    playing with JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const number = 555;
+const singer = {
+  name: "Leon",
+  age: 24,
+  address: "KHULNA",
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h3>Hello!</h3>
+      </div>
+      <div className="music">
+        <p>Name : {number + 2}</p>
+        <p>Age : {singer.age}</p>
+        <p>Address : {singer.address}</p>
+      </div>
     </div>
   );
 }
