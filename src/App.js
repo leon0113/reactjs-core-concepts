cb8a9f4c12fe993dbd0bef697ef8be4f86f33031
leon0113 <tahjib.leon399@gmail.com>
2023-01-04 23:43:08 +0600

    creating components

--- src/App.js
+++ src/App.js
@@ -11,16 +11,34 @@ const singer = {
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <h3>Hello!</h3>
-      </div>
-      <div className="music">
-        <p>Name : {number + 2}</p>
-        <p>Age : {singer.age}</p>
-        <p style={{ color: "blue", backgroundColor: "black" }}>
-          Address : {singer.address}
-        </p>
-      </div>
+      <Person></Person>
+      <Person></Person>
+      <Person1></Person1>
+      <Person1></Person1>
+      <h4>New Components :</h4>
+      <Friend></Friend>
+    </div>
+  );
+}
+
+function Person() {
+  return <h1>Shakib Al Hasan</h1>;
+}
+
+function Person1() {
+  return (
+    <div className="person">
+      <h1>Steve Smith</h1>
+      <p>Cricketer,Australia</p>
+    </div>
+  );
+}
+
+function Friend() {
+  return (
+    <div className="person">
+      <h3> Name : Leon</h3>
+      <p>Age : 24</p>
     </div>
   );
 }
