b17fdc18d22f887cfab6fea42449caabe4f4ff2d
leon0113 <tahjib.leon399@gmail.com>
2023-01-11 00:47:15 +0600

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
@@ -11,12 +11,32 @@ function App() {
 }
 
 function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
   return (
     <div>
       <h2>External Users</h2>
+      <p>{users.length}</p>
+      {
+      users.map((user) => <User name={user.name} phone={user.phone}></User>)
+      }
     </div>
   );
 }
+
+function User(props){
+  return(
+    <div style={{border:'2px solid red',margin:'20px',padding:'4px'}}>
+      <h3>Name : {props.name}</h3>
+      <p>Phone : {props.phone}</p>
+    </div>
+  )
+}
+
 
 function Counter() {
   const [count, setCount] = useState(0);
